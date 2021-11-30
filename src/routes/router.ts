5270003f83f68d11162af80003045bbdc0bb41e3
carlos quintero <info.fxstudios@gmail.com>
2021-11-30 15:05:12 -0500

    Agregado createUserWithUid

--- src/routes/router.ts
+++ src/routes/router.ts
@@ -12,4 +12,6 @@ router.post('/get-user', FirebaseController.getUser);
 
 router.post('new-user', FirebaseController.createUser);
 
+router.post('/create-user-with-uid', FirebaseController.createUserWithUid);
+
 export default router;
